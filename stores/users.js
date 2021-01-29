de0ce66582c803ddaa8cfefb760ae8a11957406f
rooiss <louis.kim010@gmail.com>
2021-01-29 14:10:03 -0800

    test 1 working for createUser

--- stores/users.js
+++ stores/users.js
@@ -1,15 +1,24 @@
+const { pick } = require('lodash')
 const users = {}
 
-const setUser = (username, userdeets) => {
-  // add the user with the deets into the users object
-  users[username] = {
-    ...userdeets,
+const getUser = () => {}
+const createUser = ({ username, password }) => {
+  // populate the users object with the username as the key
+  // the value to that key is the users metadata
+  // make sure you return a promise
+  if (!users[username]) {
+    users[username] = {
+      username: username,
+      password: password,
+      signUpTime: new Date(),
+    }
+    return Promise.resolve(pick(users[username], ['username', 'signUpTime']))
+  } else {
+    return Promise.reject('username already exists')
   }
 }
-
-const getUser = (username) => {
-  return users[username]
-}
+const updateUser = () => {}
 
 exports.getUser = getUser
-exports.setUser = setUser
+exports.createUser = createUser
+exports.updateUser = updateUser
